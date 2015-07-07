382c700e50a89af062f91b3ea337d51ad722ef39
jantea.marius <jantea.marius@outlook.com>
2015-07-07 09:43:48 +0300

    second update of modules

---
+++ testGit3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGit3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld from testGit3.1");
+        }
+    }
+}
